70c529f728c74e4b58179fa54320f048350cb59f
Tasal <tasalislam@gmail.com>
2025-10-06 14:56:48 +0100

    Created the grid and improved the alignment of the rows.
    Using a for loop in javascript enabled me to create the gride -
    I reduced the size of the width to decrease the size of grid and
    used flex-wrap so rows of 16 are formed.

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 const contaniner = document.getElementById("container")
+
+
+function createGrid(size) {
+    contaniner.innerHTML = "";
+
+    for (let i = 0; i < size * size; i++) {
+        const sqaure = document.createElement("div");
+        sqaure.classList.add("square");
+        contaniner.appendChild(sqaure);
+    }
+
+
+
+}
+
+createGrid(16)
